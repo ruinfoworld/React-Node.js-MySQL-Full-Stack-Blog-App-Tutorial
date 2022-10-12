55bd9286d078d9c6e9b4be40abea2a88131405cd
abdbodara <appdevelopment@renderhealth.com>
2022-10-12 15:19:18 +0530

    Added write page

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import EditImage from "../img/edit.png";
 import DeleteImage from "../img/delete.png"; 
 import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
     return (
@@ -21,8 +22,14 @@ const Single = () => {
                         <img src={DeleteImage} alt=""/>
                     </div>
                 </div>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus dolor sapien, ac sodales nibh pellentesque ut. </h1>
+                    <p>Duis pulvinar turpis nec metus venenatis porta. Duis ultrices arcu fringilla, malesuada justo vel, accumsan augue. Nunc rhoncus metus a suscipit dapibus. Ut egestas tortor sed velit rutrum venenatis vel sed lorem. Sed suscipit viverra scelerisque. Duis quis fringilla justo. Pellentesque facilisis dolor et eros facilisis porttitor. Etiam faucibus mi nisi, ut elementum lectus posuere eget. Suspendisse hendrerit egestas massa. Nulla id nisi in elit dictum eleifend. Vestibulum gravida vehicula tellus. Etiam vestibulum odio a accumsan scelerisque. Morbi consequat euismod vestibulum. Cras quis luctus odio.</p>
+
+                    <p>Cras rhoncus laoreet viverra. Donec rhoncus diam ornare finibus tristique. Curabitur et nibh erat. Curabitur ut magna sapien. Etiam sit amet ex vitae felis tristique congue eget vitae turpis. Maecenas eu nunc est. Quisque non interdum diam, id finibus est. Ut blandit odio eget urna aliquet, at tristique lacus volutpat. Mauris vitae quam at elit cursus placerat. Pellentesque quis ultricies ipsum. Nullam facilisis efficitur lorem, id sodales nulla viverra vel.</p>
+                    
+                    <p>Proin sit amet nisl sed ex consequat convallis sed vel odio. Aenean vitae interdum tellus, eget accumsan eros. Quisque blandit purus ac vehicula volutpat. Maecenas non ligula mi. Mauris egestas quis justo eu feugiat. Aliquam erat volutpat. Suspendisse scelerisque est et finibus consequat. Nam gravida leo vitae condimentum bibendum. Morbi a efficitur turpis. Aliquam massa dolor, rhoncus venenatis justo sit amet, rhoncus consectetur ex.</p>
             </div>
-            <div className="menu">m</div>
+            <Menu />
         </div> 
     )
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,8 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+    const [value, setValue] = useState('');
+    console.log(value,"Here we are")
     return (
-        <div>Write</div>
+        <div className="add">
+            <div className="content">
+                <input type="text" placeholder="Title"/>
+                <div className="editorContainer">
+                    <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
+                </div>
+
+            </div>
+            <div className="menu">
+                <div className="item">
+                    <h1>Published</h1>
+                    <span>
+                        <b>Status: </b> Draft
+                    </span>
+                    <span>
+                        <b>Visibility: </b> Public
+                    </span>
+                    <input style={{display:"none"}} type="file" name="" id="file"/>
+                    <label className="file" htmlFor="file">Upload Image</label>
+                    <div className="buttons">
+                        <button>Save as a draft</button>
+                        <button>Update</button>
+                    </div>
+                </div>
+                <div className="item">
+                    <h1>Category</h1>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="art" id="art"/>
+                        <label htmlFor="art">Art</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="Science" id="Science"/>
+                        <label htmlFor="Science">Science</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="Technology" id="Technology"/>
+                        <label htmlFor="Technology">Technology</label>
+                        </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="Cinema" id="Cinema"/>
+                        <label htmlFor="Cinema">Cinema</label>
+                    </div>    
+                    <div className="cat">
+                        <input type="radio" name="cat" value="Design" id="Design"/>
+                        <label htmlFor="Design">Design</label>
+                        </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="Food" id="Food"/>
+                        <label htmlFor="Food">Food</label>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
